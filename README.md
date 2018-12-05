91a098d3d212096e582bebf7ef4a99211ab62588
JJSky <jjsky111@gmail.com>
2018-12-04 23:33:15 -0700

    Hover and press "delete" to remove element, added icon, randomize color on hover

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Roadmap
 - [X] Randomize element color on user hover
 - [ ] Elements parallax based on mouse position
 - [X] Harshly offset (or delete if possible) elements focused by user hover when user presses 'del' key - Successfully deleted
+- [X] Bee icon
--- content.js
+++ content.js
@@ -1,1 +1,33 @@
-alert("Hello, the Queen Bee is currently infiltrating your webpage.");
+var booleanFromToggleWeWillNeverHave = true; //RIP
+var elm;
+
+// $('#toggleHoverShit').change(function() {
+    // booleanFromToggleWeWillNeverHave = !booleanFromToggleWeWillNeverHave;
+    if (booleanFromToggleWeWillNeverHave) {
+        $('*').mouseenter(function(el) {
+            elm = el;
+            console.log(el.target);
+            var r = Math.floor(Math.random() * 256);
+            var g = Math.floor(Math.random() * 256);
+            var b = Math.floor(Math.random() * 256);
+            var color = "rgb(" + r + "," + g + "," + b + ")";
+            var bColor = "rgb(" + b + "," + r + "," + g + ")";
+            el.target.style.color = color;
+            el.target.style.backgroundColor = bColor;
+            console.log(color);
+        });
+        $('*').mouseleave(function() {
+            $('*').unbind('keypress');
+        });
+        $(document).keydown(function(e) {
+            console.log('deleteme daddy OwO');
+            if(e.which == 46) {
+                console.log('pressed delet daddy ');
+                elm.target.parentElement.removeChild(elm.target);
+            }
+        });
+    } else {
+        $('*').unbind('mouseenter mouseleave');
+    }
+// });
+//"content_security_policy": "script-src 'self' 'sha256-Q2bW3UTLYaxRrDcGIx1T5eEOeSAeThHWF7akXNOiN7Q='",
